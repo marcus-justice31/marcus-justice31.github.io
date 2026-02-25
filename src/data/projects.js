e2559d4cfc545cc0f10a7b94df02a987bcd4c307
marcus-justice31 <marcusuy@crescentschool.org>
2026-02-25 10:52:19 -0500

    added more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,11 @@
 import mealDashboardImg from "../assets/banners/ai_meal_dashboard.png";
 import smartSortingUiImg from "../assets/banners/smartsorting_ui.png";
 import smartParkingImg from "../assets/banners/SmartParkingDashboard.png";
+import ecommerceUI from "../assets/banners/ecommerceUI.png";
+// import ecommerceUI2 from "../assets/banners/ecommerceUI2.png";
+import cpu_implementation from "../assets/banners/cpu_implementation.png";
+import p2p_3peersDownload from "../assets/banners/p2p_3peersDownload.png"
+import onlineOrderUI from "../assets/banners/onlineOrderUI.png";
 
 const projects = [
   {
@@ -57,7 +62,7 @@ const projects = [
     id: 3,
     title: "Smart Parking System",
     description:
-      "A a cloud-native, smart parking system that simulates IoT sensors to monitor parking space availability in real-time.",
+      "A cloud-native, smart parking system that simulates IoT sensors to monitor parking space availability in real-time.",
     image: smartParkingImg,
     tags: [
       { name: "React", color: "#49d2f8ff" },
@@ -77,6 +82,76 @@ const projects = [
       "Developed 10+ backend API endpoints for user authentication, dynamic pricing, wallet management, and admin controls, improving system scalability and usability",
       "Utilizes RabbitMQ message-broker software to enable asynchronous communication and prevents multiple users from purchasing a parking spot at the same time (race condition)",
       "Implemented dynamic pricing based on the time of the day (higher pricing during peak hours)"
+    ],
+  },
+
+  {
+    id: 4,
+    title: "Online Order Express React",
+    description:
+      "An online ordering system for food items that has a functioning PayPal checkout system.",
+    image: onlineOrderUI,
+    tags: [
+      { name: "React", color: "#49d2f8ff" },
+      { name: "Python", color: "#0283d3ff" },
+      { name: "JavaScript", color: "#f7df1e" },
+      { name: "HTML", color: "#f7781eff" },
+      { name: "CSS", color: "#9d1ef7ff" },
+      { name: "Express JS", color: "#f7df1e" },
+      { name: "MongoDB", color: "#00684A" },
+    ],
+    github: "https://github.com/marcus-justice31/OnlineOrder-ExpressReact",
+    details: [
+      "loem",
+    ],
+  },
+
+
+  {
+    id: 5,
+    title: "E-Commerce Application",
+    description:
+      "An E-Commerce DBMS where users can buy/sell items, developed using Java and Oracle SQL.",
+    image: ecommerceUI,
+    tags: [
+      { name: "Java", color: "#f7781eff" },
+      { name: "JavaFX", color: "#9d1ef7ff" },
+      { name: "Oracle SQL", color: "#00684A" }
+    ],
+    github: "https://github.com/marcus-justice31/E-Commerce-App",
+    details: [
+      "fxixx",
+    ],
+  },
+
+  {
+    id: 6,
+    title: "Peer-to-Peer Application",
+    description:
+      "A Peer-to-Peer (P2P) file-sharing application using socket programming.",
+    image: p2p_3peersDownload,
+    tags: [
+      { name: "C", color: "#f7781eff" },
+      { name: "Computer Networking", color: "#7e7e7eff" },
+    ],
+    github: "https://github.com/marcus-justice31/Peer-to-Peer-Application",
+    details: [
+      "fxixx",
+    ],
+  },
+
+  {
+    id: 7,
+    title: "CPU Implementation using VHDL",
+    description:
+      "A CPU implementation that can perform 26 different mnemonic operations",
+    image: cpu_implementation,
+    tags: [
+      { name: "VHDL", color: "#f73b1eff" }
+    ],
+    github: "https://github.com/marcus-justice31/E-Commerce-App",
+    details: [
+      "*fixxx*",
     ],
   },
 ];
